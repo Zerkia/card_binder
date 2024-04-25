0902f2cfa9c31033a12dbb8a4b77afa4e03b6ad4
Zerkia <lordzerkia@gmail.com>
2024-04-25 14:55:36 +0200

    added dropdown for sets to aquire all releases

--- src/components/SpecificCard.tsx
+++ src/components/SpecificCard.tsx
@@ -1,24 +1,48 @@
 import useCard from "@/hooks/useCard";
 import Image from "next/image";
+import { Roboto } from "next/font/google";
 
-interface SpecificCardProps{
-    id: number
+interface SpecificCardProps {
+  id: number;
 }
 
-export default function SpecificCard({ id }: SpecificCardProps) {
-    const { card } = useCard(id)
+const roboto = Roboto({
+  weight: ["100", "400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
-    return (
-        <>
-        { card ?
-            <section>
-            <h1>{ card.name }</h1>
-            <h1>{ card.card_sets[0].set_code.split('-')[0] }</h1>
-            {/* <Image src={card.card_images[0].image_url} alt={card.name} width={50} height={50}></Image> */}
-            </section>
-            : 
-            <p>No card found</p>
-        }
-        </>
-    )
+export default function SpecificCard({ id }: SpecificCardProps) {
+  const { card } = useCard(id);
+
+  return (
+    <>
+      {card ? (
+        <section className="text-center">
+          <h1 className="text-black">{card.name}</h1>
+          <select
+            id="cardSets"
+            className={
+              roboto.className +
+              ` block mx-auto bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`
+            }
+          >
+            <option selected>Choose a set</option>
+            {card.card_sets.map((cardSet) => (
+              <option
+                key={cardSet.set_code}
+                value={cardSet.set_code}
+                className={roboto.className}
+              >
+                {cardSet.set_code.split("-")[0]}
+              </option>
+            ))}
+          </select>
+          {/* <Image src={card.card_images[0].image_url} alt={card.name} width={50} height={50}></Image> */}
+        </section>
+      ) : (
+        <p>No card found</p>
+      )}
+    </>
+  );
 }
