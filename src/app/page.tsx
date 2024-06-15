bc23d4530fbdb50b802238e92e1d8c30c00dca59
MeeWNikolajP <lordzerkia@gmail.com>
2024-06-15 16:38:31 +0200

    code cleanup, light QOL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,16 @@ import CardList from "@/components/CardList";
 import CardSearch from "@/components/CardSearch";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./theme";
 
 
 export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col px-12 pt-6">
-      <ChakraProvider theme={theme}>
-        <Header />
-        <CardSearch />
-        <CardList />
-        <Footer />
-      </ChakraProvider>
+      <Header />
+      <CardSearch />
+      <CardList />
+      <Footer />
     </main>
   );
 }
--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -9,12 +9,17 @@ export default function CardList() {
 return (
     <section>
         { cards && cards.slice(0, 25).map((card) => 
-        <Link key={card.id} href={`/card/${card.id}`}>
-            <button 
-            type="button" 
-            className="block text-white bg-blue-700 hover:bg-blue-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
-            w-full">{card.name}</button>
-        </Link>
+            <Link key={card.id} href={`/card/${card.id}`}>
+                <button 
+                type="button" 
+                className="block text-white bg-blue-700 hover:bg-blue-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
+                w-full">{card.name}</button>
+            </Link>
+        )}
+        {cards && (
+            <p className="text-center">
+                Pick from a total of <span className="font-bold italic">{cards.length}</span> cards to add to your collection!
+            </p>
         )}
     </section>
     )
